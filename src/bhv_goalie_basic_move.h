4710699126fce3a6fe814a3481b96edb753a4515
nader.zare <nader.zare@peykasa.ir>
2017-10-17 17:49:41 +0330

    simpling golie basic move

--- src/bhv_goalie_basic_move.h
+++ src/bhv_goalie_basic_move.h
@@ -48,31 +48,6 @@ public:
 private:
 
     rcsc::Vector2D getTargetPoint( rcsc::PlayerAgent * agent );
-    double getBasicDashPower( rcsc::PlayerAgent * agent,
-                              const rcsc::Vector2D & move_point );
-
-    bool doPrepareDeepCross( rcsc::PlayerAgent * agent,
-                             const rcsc::Vector2D & move_point );
-    bool doStopAtMovePoint( rcsc::PlayerAgent * agent,
-                            const rcsc::Vector2D & move_point );
-    bool doMoveForDangerousState( rcsc::PlayerAgent * agent,
-                                  const rcsc::Vector2D & move_point );
-    bool doCorrectX( rcsc::PlayerAgent * agent,
-                     const rcsc::Vector2D & move_point );
-    bool doCorrectBodyDir( rcsc::PlayerAgent * agent,
-                           const rcsc::Vector2D & move_point,
-                           const bool consider_opp );
-    bool doGoToMovePoint( rcsc::PlayerAgent * agent,
-                          const rcsc::Vector2D & move_point );
-
-
-    void doGoToPointLookBall( rcsc::PlayerAgent * agent,
-                              const rcsc::Vector2D & target_point,
-                              const rcsc::AngleDeg & body_angle,
-                              const double & dist_thr,
-                              const double & dash_power,
-                              const double & back_power_rate = 0.7 );
-
 };
 
 #endif
